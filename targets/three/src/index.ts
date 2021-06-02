49b6b5bee40ed7a1f36f450d0a7c8732b8b4845a
David Lindkvist <david@14islands.com>
2021-06-02 12:55:33 +0200

    fix(three): disable three FrameLoop and monkey patch rafz animation frame to use r3f

--- targets/three/src/index.ts
+++ targets/three/src/index.ts
@@ -2,22 +2,26 @@ import { applyProps, addEffect, invalidate } from '@react-three/fiber'
 import { Globals } from '@react-spring/core'
 import { createStringInterpolator, colors } from '@react-spring/shared'
 import { createHost } from '@react-spring/animated'
-import { FrameLoop } from './FrameLoop'
+// import { FrameLoop } from './FrameLoop'
 import { primitives } from './primitives'
 import { WithAnimated } from './animated'
 
+let nextFrame: Function | undefined = undefined
+
 // Let r3f drive the frameloop.
-const frameLoop = new FrameLoop(() => invalidate())
+// const frameLoop = new FrameLoop(() => invalidate())
 addEffect(() => {
-  frameLoop.advance()
+  // frameLoop.advance()
+  if (nextFrame) nextFrame()
   return true // Never stop.
 })
 
 Globals.assign({
   createStringInterpolator,
   colors,
-  //@ts-ignore
-  frameLoop,
+  // @ts-ignore
+  // frameLoop,
+  requestAnimationFrame: cb => (nextFrame = cb),
 })
 
 const host = createHost(primitives, {
